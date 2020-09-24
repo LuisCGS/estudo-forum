a0cf3541ae6cd467f3221463c86b1db5139aa477
Luis C. G. Sanches <luis.cgs.fatec@gmail.com>
2020-09-24 17:48:23 -0300

    Adicionado configuração para exibir querys, adicionado modelos de querys na interface e métodos de request com parametros

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,26 +1,38 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
-import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/topico")
 public class TopicosController {
     @Autowired //injeção de dependencia
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
-    public List<TopicoDto> lista(){
+//    @GetMapping("/topicos/{nomeCurso}")
+//    public List<TopicoDto> lista(@PathVariable(value = "nomeCurso", required = false) final String nomeCurso){
+
+//    http://localhost:8080/topico?nomeCurso=abc
+//    @GetMapping()
+//    public List<TopicoDto> lista(String nomeCurso){}
+
+    @RequestMapping({"/lista/{nomeCurso}", "/lista"})
+    public List<TopicoDto> lista(@PathVariable(value = "nomeCurso", required = false) Optional<String> nomeCurso){
+        System.out.println(nomeCurso);
 //        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring","Programação"));
 //        return TopicoDto.converter(Arrays.asList(topico, topico));
 
-        return TopicoDto.converter(topicoRepository.findAll());
+        if (nomeCurso.isEmpty()) {
+            return TopicoDto.converter(topicoRepository.findAll());
+        } else {
+            return TopicoDto.converter(topicoRepository.findByCurso_Nome(nomeCurso.get()));
+        }
     }
 }
--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -2,7 +2,17 @@ package br.com.alura.forum.repository;
 
 import br.com.alura.forum.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long/* tipo do id da classe topico*/>  {
 
+    //List<Topico> findByTitulo(String nomeCurso);
+    List<Topico> findByCurso_Nome(String nomeCurso);//curso é o relacionamento e nome é o atributo de curso
+    // Quando o atributo da classe conflitar com um relacionamento e atributo utilizar o _ pra mostrar que sera de uma classe o relacionamento
+
+    @Query("SELECT t FROM Topico t WHERE t.curso.nome = : nomeCurso")
+    List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
 }
