7de47845b2733a6d3f6f57763c4500be1e5e7394
Luis C. G. Sanches <luis.cgs.fatec@gmail.com>
2020-09-25 11:41:38 -0300

    Adicionado o @Transaction nos metodos de remover e cadastrar (evitar futuros erros), e implementação do remover

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -50,6 +50,7 @@ public class TopicosController {
 
     //Deve devolver 201 que significa SUCESSO / CRIADO
     @PostMapping("/cadastrar")
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
         Topico topico = topicoForm.converter(cursoRepository);
         topicoRepository.save(topico);
@@ -73,5 +74,13 @@ public class TopicosController {
         Topico topico = topicoForm.topicoComNovosDados(id, topicoRepository);
 
         return ResponseEntity.ok(new TopicoDto(topico));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id) {
+        topicoRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
 }
