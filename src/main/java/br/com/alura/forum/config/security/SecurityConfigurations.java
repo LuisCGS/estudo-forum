6449d0f0eb8e58f42019d91d3c318579d8479a5b
Luis C. G. Sanches <luis.cgs.fatec@gmail.com>
2020-09-28 15:26:05 -0300

    Adicionado as configurações do Spring Security e feito a liberação das urls de consulta na classe SecurityConfigurations

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // Habilita o modulo de segurança na aplicação
+@Configuration // Diz que no startup da aplicação o Spring irá carregar e ler as infos desta class
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+    // Configurações de autorização (URLs, perfis de acesso)
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topico").permitAll()
+                .antMatchers(HttpMethod.GET, "/topico/*").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    // Configurações de autenticação (login)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    // Configuraões de recursos estaticos (js, CSS, imagens)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+}
