2cd78f5e2b2792c828e7be0a7a0a08b5dc35c699
Luis C. G. Sanches <luis.cgs.fatec@gmail.com>
2020-10-07 17:13:36 -0300

    Efetuando o build da aplicação para WAR, não afeta em nada o desenvolvimento

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,6 +2,8 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,10 +12,17 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport // habilitar o suporte para o spring pegar os parametros da url informações da ordenação e paginacao e repassar para o spring data
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication {
+
+public class ForumApplication
+		extends SpringBootServletInitializer /* Habilita a configuração da Servlet 3.0 dentro do servidor, utilizar quando for gerar um WAR */ {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ForumApplication.class); // Utilizar apenas quando for gerar um WAR
+	}
 }
